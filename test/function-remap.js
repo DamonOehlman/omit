eb64c8ee56a0a3073e202d4cffa825f4a9ec70db
Damon Oehlman <damon.oehlman@sidelab.com>
2013-03-08 16:31:23 +1000

    Finished evaluator remapping tests

--- test/function-remap.js
+++ test/function-remap.js
@@ -46,30 +46,24 @@ describe('evaluator based value remapping', function() {
         });
     });
 
-    /*
     describe('standalone object', function() {
-        it('should be able to remove details based on key name', function() {
-            var entry = omit(sample.entries[0], removeCreated);
-
-            // iterate through the entries, and validate that no created by values slipped through
-            assert(! entry.created, 'Entry has created information');
+        it('should be able to update details based on key name', function() {
+            var entry = omit(sample.entries[0], zeroCreated);
+            assert.equal(entry.created, 0, 'created value was not updated');
         });
 
-        it('should be able to remove two keys', function() {
-            var entry = omit(sample.entries[0], [removeCreated, removeLastModified]);
+        it('should be able to zero two keys', function() {
+            var entry = omit(sample.entries[0], [zeroCreated, zeroLastModified]);
 
-            // iterate through the entries, and validate that no created by values slipped through
-            assert(! entry.created, 'Entry has created information');
-            assert(! entry.last_modified, 'Entry has last_modified information');
+            assert.equal(entry.created, 0, 'created value not updated');
+            assert.equal(entry.last_modified, 0, 'last_modified not updated');
         });
 
-        it('should be able to remove two keys (varargs variant)', function() {
-            var entry = omit(sample.entries[0], removeCreated, removeLastModified);
+        it('should be able to zero two keys (varargs variant)', function() {
+            var entry = omit(sample.entries[0], zeroCreated, zeroLastModified);
 
-            // iterate through the entries, and validate that no created by values slipped through
-            assert(! entry.created, 'Entry has created information');
-            assert(! entry.last_modified, 'Entry has last_modified information');
+            assert.equal(entry.created, 0, 'created value not updated');
+            assert.equal(entry.last_modified, 0, 'last_modified not updated');
         });
     });
-*/
 });
