75fe6e85a29ff288772ebf148f0915293d8b8e12
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-05 21:45:10 +1000

    Adding lint

--- index.js
+++ index.js
@@ -1,79 +1,82 @@
-function omit(input, entries) {
-    var notArray = !Array.isArray(input),
-        output = [],
-        itemIdx,
-        entryIdx,
-        key,
-        reject,
-        value,
-        entry,
-        entryCount,
-        testObject,
-        newObject;
+/* jshint node: true */
+'use strict';
 
-    // if the input is not an object, then simply passthrough
-    if (typeof input != 'object' || (input instanceof String)) {
-        return input;
-    }
+module.exports = function(input, entries) {
+  var notArray = !Array.isArray(input);
+  var output = [];
+  var itemIdx;
+  var entryIdx;
+  var key;
+  var reject;
+  var value;
+  var entry;
+  var entryCount;
+  var testObject;
+  var newObject;
 
-    // if the data is not an array, then wrap in an array for consistent treatment
-    if (notArray) {
-        input = [input];
-    }
+  // if the input is not an object, then simply passthrough
+  if (typeof input != 'object' || (input instanceof String)) {
+    return input;
+  }
 
-    // ensure entries is an array
-    entries = [].concat(entries || []).concat(Array.prototype.slice.call(arguments, 2));
+  // if the data is not an array, then wrap in an array for consistent treatment
+  if (notArray) {
+    input = [input];
+  }
 
-    // initialise the entry count
-    entryCount = entries.length;
+  // ensure entries is an array
+  entries = [].concat(entries || [])
+    .concat(Array.prototype.slice.call(arguments, 2));
 
-    // iterate through the elements in the data and 
-    for (itemIdx = input.length; itemIdx--; ) {
-        // initialise the test object and new object
-        testObject = input[itemIdx];
-        newObject = output[itemIdx] = {};
+  // initialise the entry count
+  entryCount = entries.length;
 
-        // iterate through the keys in the object
-        for (key in testObject) {
-            if (testObject.hasOwnProperty(key)) {
-                value = testObject[key];
-                reject = false;
+  // iterate through the elements in the data and 
+  for (itemIdx = input.length; itemIdx--; ) {
+    // initialise the test object and new object
+    testObject = input[itemIdx];
+    newObject = output[itemIdx] = {};
 
-                // iterate through the entry tests
-                for (entryIdx = 0; (! reject) && entryIdx < entryCount; entryIdx++) {
-                    entry = entries[entryIdx];
+    // iterate through the keys in the object
+    for (key in testObject) {
+      if (testObject.hasOwnProperty(key)) {
+        value = testObject[key];
+        reject = false;
 
-                    // if the entry is a string, then omit only if we have a key match
-                    switch (typeof entry) {
-                        case 'function':
-                            reject = entry(key, value, testObject);
+        // iterate through the entry tests
+        for (entryIdx = 0; (! reject) && entryIdx < entryCount; entryIdx++) {
+          entry = entries[entryIdx];
 
-                            // check for value updates
-                            if (typeof reject == 'object' && typeof reject.newValue != 'undefined') {
-                                value = reject.newValue;
-                                reject = false;
-                            }
+          // if the entry is a string, then omit only if we have a key match
+          switch (typeof entry) {
+          case 'function':
+            reject = entry(key, value, testObject);
 
-                            // ensure reject is a boolean value
-                            reject = reject === true;
-
-                            break;
-
-                        case 'string':
-                            reject = key === entry;
-                            break;
-                    }
-                }
-
-                // if we are not rejecting the key, then add the value to the new object
-                if (! reject) {
-                    newObject[key] = value;
-                }
+            // check for value updates
+            if (typeof reject == 'object' &&
+              typeof reject.newValue != 'undefined') {
+              value = reject.newValue;
+              reject = false;
             }
+
+            // ensure reject is a boolean value
+            reject = reject === true;
+
+            break;
+
+          case 'string':
+            reject = key === entry;
+            break;
+          }
         }
+
+        // if we are not rejecting the key, then add the value to the new object
+        if (! reject) {
+          newObject[key] = value;
+        }
+      }
     }
+  }
 
-    return notArray ? output[0] : output;
-}
-
-module.exports = omit;
+  return notArray ? output[0] : output;
+};
